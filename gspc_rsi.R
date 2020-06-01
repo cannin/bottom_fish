6ffd717bbad3425b694e9636525810926dabd1c3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-01 04:40:38 +0000

    Deploy cannin/bottom_fish to github.com/cannin/bottom_fish.git:gh-pages

--- gspc_rsi.R
+++ gspc_rsi.R
@@ -1,4 +1,5 @@
 library(quantmod)
+library(jsonlite)
 
 dat <- getSymbols("^GSPC", auto.assign = FALSE, from = "2020-01-02")
 fpr <- Cl(dat)
@@ -6,5 +7,10 @@ rsi <- RSI(fpr)
 
 tmp <- as.data.frame(rsi)
 df <- data.frame(date=rownames(tmp), rsi=tmp$rsi)
+df <- tail(df, 20)
 
 write.table(df, "gspc.txt", sep="\t", quote=FALSE, row.names = FALSE)
+
+json <- toJSON(df, digits=2)
+write(json, "gspc.json")
+
