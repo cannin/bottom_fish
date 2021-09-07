31c8137501654d4d68f5e26dd13fc81af7f97afb
cannin <cannin@gmail.com>
2021-09-07 10:26:03 -0400

    Add result data.frame

--- daily_rsi.R
+++ daily_rsi.R
@@ -79,17 +79,20 @@ for(i in 2:(2+length(symbols)-1)) {
 
 s4 <- s3[nrow(s3), 2:(2+length(symbols)-1)]
 i3 <- which(!is.na(s4))
+
 vals <- unlist(s4[i3])
 names(vals) <- gsub(".rsi", "", names(vals))
+vals_df <- data.frame(symbol=gsub(".rsi", "", names(vals)), vals=vals, stringsAsFactors=FALSE)
+
 last_date <- s3$VTI.date[nrow(s3)]
 
 vals_str <- paste(names(vals), vals, collapse="_", sep=":")
 vals_str <- paste0(vals_str, "_", last_date)
 
-lst <- list(run_time=Sys.time(), date=last_date, hi_rsi=hi_rsi, lo_rsi=lo_rsi, result_str=vals_str)
+lst <- list(run_time=Sys.time(), date=last_date, hi_rsi=hi_rsi, lo_rsi=lo_rsi, result_str=vals_str, result_df=vals_df)
 
 # WRITE RESULTS ----
-json <- toJSON(lst, digits=2, auto_unbox=TRUE)
+json <- toJSON(lst, digits=2, auto_unbox=TRUE, rownames=FALSE)
 write(json, "rsi.json")
 
 cat(dir())
