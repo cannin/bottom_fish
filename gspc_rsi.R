9af5f0d196d8d5716f431fc75d30f7d10e81b3d9
cannin <cannin@gmail.com>
2022-08-02 17:25:37 +0000

    Deploying to gh-pages from @ cannin/bottom_fish@b7da5edd8571feeec86b2a55e777eb366f5f02ab 🚀

--- gspc_rsi.R
+++ gspc_rsi.R
@@ -2,11 +2,12 @@ library(quantmod)
 library(jsonlite)
 
 dat <- getSymbols("^GSPC", auto.assign = FALSE, from = "2020-01-02")
-fpr <- Cl(dat)
-rsi <- RSI(fpr)
+cl <- Cl(dat)
+rsi <- RSI(cl)
 
 tmp <- as.data.frame(rsi)
-df <- data.frame(date=rownames(tmp), rsi=tmp$rsi)
+df <- data.frame(date=rownames(tmp), rsi=tmp$rsi, close=cl)
+colnames(df) <- c("date", "rsi", "close")
 df <- tail(df, 20)
 
 write.table(df, "gspc.txt", sep="\t", quote=FALSE, row.names = FALSE)
