4c224b4df860216ced18f01b3f05c0af5cdd6f12
cannin <cannin@gmail.com>
2022-08-02 13:34:00 -0400

    Fixed close date name

--- gspc_rsi.R
+++ gspc_rsi.R
@@ -6,8 +6,7 @@ cl <- Cl(dat)
 rsi <- RSI(cl)
 
 tmp <- as.data.frame(rsi)
-df <- data.frame(date=rownames(tmp), rsi=tmp$rsi, close=cl)
-colnames(df) <- c("date", "rsi", "close")
+df <- data.frame(date=rownames(tmp), rsi=tmp$rsi, close=as.vector(cl))
 df <- tail(df, 20)
 
 write.table(df, "gspc.txt", sep="\t", quote=FALSE, row.names = FALSE)
