a1d15f38ecd55ec4e3988aabf127279c129c3adf
cannin <cannin@gmail.com>
2022-08-02 17:42:25 +0000

    Deploying to gh-pages from @ cannin/bottom_fish@f993be90e2fe238cf6333d55702b3ede1a841fd9 🚀

--- gspc_rsi.R
+++ gspc_rsi.R
@@ -1,17 +1,25 @@
 library(quantmod)
 library(jsonlite)
 
-dat <- getSymbols("^GSPC", auto.assign = FALSE, from = "2020-01-02")
-cl <- Cl(dat)
-rsi <- RSI(cl)
+symbols <- c("gspc"="^GSPC", "vti"="VTI")
 
-tmp <- as.data.frame(rsi)
-df <- data.frame(date=rownames(tmp), rsi=tmp$rsi, close=as.vector(cl))
-df <- tail(df, 20)
-
-write.table(df, "gspc.txt", sep="\t", quote=FALSE, row.names = FALSE)
-
-json <- toJSON(df, digits=2)
-write(json, "gspc.json")
+for(i in 1:length(symbols)) {
+  #i <- 1
+  symbol <- unname(symbols[i])
+  name <- names(symbols)[i]
+  
+  dat <- getSymbols(symbol, auto.assign = FALSE, from = "2020-01-02")
+  cl <- Cl(dat)
+  rsi <- RSI(cl)
+  
+  tmp <- as.data.frame(rsi)
+  df <- data.frame(date=rownames(tmp), rsi=tmp$rsi, close=as.vector(cl))
+  df <- tail(df, 20)
+  
+  write.table(df, paste0(name, ".txt"), sep="\t", quote=FALSE, row.names = FALSE)
+  
+  json <- toJSON(df, digits=2)
+  write(json, paste0(name, ".json"))
+}
 
 cat(dir())
