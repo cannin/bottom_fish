9317aa038d8ec77fb1aa6c39ab7b356179008d33
cannin <cannin@gmail.com>
2022-08-22 07:48:00 -0400

    Added target RSI

--- gspc_rsi.R
+++ gspc_rsi.R
@@ -4,7 +4,7 @@ library(jsonlite)
 symbols <- c("gspc"="^GSPC", "vti"="VTI")
 
 for(i in 1:length(symbols)) {
-  #i <- 1
+  #i <- 2
   symbol <- unname(symbols[i])
   name <- names(symbols)[i]
   
@@ -16,6 +16,11 @@ for(i in 1:length(symbols)) {
   df <- data.frame(date=rownames(tmp), rsi=round(tmp$rsi, 2), close=round(as.vector(cl), 2))
   df$close_target <- round(df$close*0.99, 2)
   df <- tail(df, 20)
+  
+  t1 <- c(as.numeric(cl$VTI.Close), df$close_target[nrow(df)])
+  t1 <- c(as.numeric(cl$VTI.Close), df$close_target[nrow(df)])
+  t2 <- RSI(t1)
+  df$rsi_target <- c(rep(NA, 19), t2[length(t2)])
   
   write.table(df, paste0(name, ".txt"), sep="\t", quote=FALSE, row.names = FALSE)
   
