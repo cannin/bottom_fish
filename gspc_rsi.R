9d875b8a5adb9538ec740f09a894f3af53cea908
cannin <cannin@gmail.com>
2022-08-22 11:54:56 +0000

    Deploying to gh-pages from @ cannin/bottom_fish@e92d0b6e00b6e563d748015dd6a20e5f866560ed 🚀

--- gspc_rsi.R
+++ gspc_rsi.R
@@ -17,6 +17,12 @@ for(i in 1:length(symbols)) {
   df$close_target <- round(df$close*0.99, 2)
   df <- tail(df, 20)
   
+  if(name != "gspc") {
+    t1 <- c(as.numeric(cl$VTI.Close), df$close_target[nrow(df)])
+    t2 <- RSI(t1)
+    df$rsi_target <- c(rep(NA, 19), t2[length(t2)])    
+  }
+
   write.table(df, paste0(name, ".txt"), sep="\t", quote=FALSE, row.names = FALSE)
   
   json <- toJSON(df, digits=2)
